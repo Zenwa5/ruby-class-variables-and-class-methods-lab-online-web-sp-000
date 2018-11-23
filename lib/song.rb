1f0212f91cca0e461a19a964258b7421a90e5df0
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-23 20:37:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,4 +32,26 @@ class Song
     @@artists.uniq
   end
 
+
+  def self.genre_count
+      genre_count_hash = {}
+       @@genres.uniq.each do |genre|
+        genre_count_hash[genre] = 0
+      end
+       @@genres.each do |genre|
+        genre_count_hash[genre] += 1
+      end
+        return genre_count_hash
+    end
+     def self.artist_count
+      artist_count_hash = {}
+       @@artists.uniq.each do |artist|
+        artist_count_hash[artist] = 0
+      end
+       @@artists.each do |artist|
+        artist_count_hash[artist] += 1
+      end
+        return artist_count_hash
+    end
+   end
 end
