44775e9b566f1a0d041dfd8ea95e570b777cdfd0
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-23 20:27:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@genres = []
 
   def initialize
    @@count += 1
@@ -13,7 +14,7 @@ class Song
     @@count
   end
 
-  @@genres = []
+
 
 
 end
